3438268ad195bd423a423d0ee7e2be3a48700b22
Margokomilfo-dev <margokomilfo@mail.ru>
2022-09-07 13:26:27 +0200

    types

--- src/ability/types/types.ts
+++ src/ability/types/types.ts
@@ -1,7 +1,7 @@
 import { Ability, InferSubjects } from '@casl/ability';
-import { User } from '../user/entities/user.entity';
+import { User } from '../../user/entities/user.entity';
 import { ActionsEnum } from '../enums/enums';
 
 // export type Subjects = InferSubjects<typeof User | typeof Post> | 'all';
 export type SubjectsType = InferSubjects<typeof User> | 'all';
-export type AppAbility = Ability<[ActionsEnum, SubjectsType]>;
+export type AppAbilityType = Ability<[ActionsEnum, SubjectsType]>;
